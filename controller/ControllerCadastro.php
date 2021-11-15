2c77eb4fb3da02db6e275796ca6b3981b813604f
João Gabriel Sá de Olvieira <89820480+gabrielf0rlen@users.noreply.github.com>
2021-11-15 15:57:31 -0300

    Update ControllerCadastro.php

--- controller/ControllerCadastro.php
+++ controller/ControllerCadastro.php
@@ -1,6 +1,6 @@
 <?php
 $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-require_once("$root/html-bd-proj/model/cadastro.php");
+require_once("$root/projeto-htmlsql/model/cadastro.php");
 
 class cadastroController{
 
