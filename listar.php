2d4fa5fc1631df66960283dd5b86f34720f5ee6d
João Gabriel Sá de Olvieira <89820480+gabrielf0rlen@users.noreply.github.com>
2021-11-15 15:59:10 -0300

    Update listar.php

--- listar.php
+++ listar.php
@@ -26,7 +26,7 @@ require_once("controller/ControllerCadastro.php");
 <body> 
 		<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #181818;">
             <div class="container-fluid">
-			<a class="navbar-brand" href="index.php">WEB Agendamentos</a>
+			<a class="navbar-brand" href="index.php">Listar</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
